0505e1a2cae9f73b7af7bfd06047cc9e8fb9a9f1
MW0MWZ <66065904+MW0MWZ@users.noreply.github.com>
2020-07-07 10:03:03 +0100

    Update autostream.py

--- sbin/autostream.py
+++ sbin/autostream.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python
 
+#   Copyright (C) 2020 by Andrew Taylor (MW0MWZ)
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
 ###############################################################################
 #                                                                             #
 #                        RigPi Auto Streaming Control                         #
@@ -26,7 +42,7 @@ callsign = "M1ABC"
 code = "fffffffffff"
 
 # Streaming Destination
-streamDest = "http://1.1.1.1:8080/stream.ffm"
+streamDest = "http://1.1.1.1/stream.ffm"
 
 # Log file
 outputLog = "/var/log/autostream.log"
@@ -107,7 +123,7 @@ while True: #Main loop
 			# Output
 			output = "OnAir: " + freqOut + suffix + ' ' + mode
 			if not "error" in output:
-				# Post Radio Data to www.mw0mwz.co.uk
+				# Post Radio Data to HRDLog.net
 				timeStamp = time.time()
 				url = 'http://robot.hrdlog.net/OnAir.aspx'
 				myobj = {'Frequency': freqNum, 'Mode': mode, 'Radio': radio, 'Callsign': callsign, 'Code': code}
